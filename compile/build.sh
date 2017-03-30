afe709065637ef755c11b7a94ddd17c41e89dec8
Peter Jensen <peter.jensen@intel.com>
2017-03-29 18:30:50 -0700

    Added finished build of convnet.js for convenience

---
+++ compile/build.sh
@@ -0,0 +1,18 @@
+cat \
+../src/convnet_init.js \
+../src/convnet_util.js \
+../src/convnet_vol.js \
+../src/convnet_vol_util.js \
+../src/convnet_layers_dotproducts.js \
+../src/convnet_layers_pool.js \
+../src/convnet_layers_input.js \
+../src/convnet_layers_loss.js \
+../src/convnet_layers_nonlinearities.js \
+../src/convnet_layers_dropout.js \
+../src/convnet_layers_normalization.js \
+../src/convnet_net.js \
+../src/convnet_trainers.js \
+../src/convnet_magicnet.js \
+../src/convnet_export.js \
+>../build/convnet.org.js
+
